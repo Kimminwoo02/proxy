eb562d8c82c36c83846be9ab7babe7482133fe4a
minu <made_power@naver.com>
2023-05-22 01:27:22 +0900

    구현체 클래스 기반 프록시 적용
    애플리케이션에 구현체 기반 클래스를 적용시켜보았다. 여전히 중복문제와 사용하지 않는 부모클래스의 소스를 상속받아와서 값을 넣어줘야하는 문제가 존재한다.

--- src/main/java/hello/proxy/app/v2/OrderServiceV2.java
+++ src/main/java/hello/proxy/app/v2/OrderServiceV2.java
@@ -1,9 +1,10 @@
 package hello.proxy.app.v2;
 
-import hello.proxy.app.v2.OrderRepositoryV2;
 
 public class OrderServiceV2 {
     private final OrderRepositoryV2 orderRepositoryV2;
+
+
 
     public OrderServiceV2(OrderRepositoryV2 orderRepositoryV2) {
         this.orderRepositoryV2 = orderRepositoryV2;
