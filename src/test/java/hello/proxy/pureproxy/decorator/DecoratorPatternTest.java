c4476dd88bae6d3a0361b78fcb46390f57ba9f29
minu <made_power@naver.com>
2023-05-21 19:06:15 +0900

    데코레이터 패턴 예제v3
    *** 기능 외에 추가적으로 시간을 측정하는 기능을 작성해서 호출해보았다.

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -4,6 +4,7 @@ import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.proxy.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +23,14 @@ public class DecoratorPatternTest {
         Component realComponent = new RealComponent();
         Component messageDeco = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDeco);
+        client.execute();
+    }
+    @Test
+    void decorator2(){
+        Component realComponent = new RealComponent();
+        Component messageDeco = new MessageDecorator(realComponent);
+        Component timeDeco = new TimeDecorator(messageDeco);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDeco);
         client.execute();
     }
 }
