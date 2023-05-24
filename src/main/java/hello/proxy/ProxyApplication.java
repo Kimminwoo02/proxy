35356508f1d9a20907865fae9ae423e20a474840
minu <made_power@naver.com>
2023-05-24 13:30:08 +0900

    빈 후처리기를 이용한 프록시 등록
    기존에 Config에서 프록시 등록을 위한 중복되는 코드를 작성했어야 했다. 빈 후처리기를 사용한 이후에는 빈 후처리기에 프록시를 등록하면 자동으로 필요한 곳에 프록시를 넣어준다.

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
-import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV1;
-import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +10,7 @@ import org.springframework.context.annotation.Import;
 
 //@Import(AppV2Config.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
