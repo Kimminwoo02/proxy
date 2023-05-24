f5a2e685c592f68086b6231c50b96deaf9d7658c
minu <made_power@naver.com>
2023-05-24 13:58:26 +0900

    스프링이 제공하는 빈 후처리기
    직접 만들었던 후처리기 대신해서 스프링이 기본적으로 제공하는 빈 후처리기를 사용하여 애플리케이션을 실행시켜보았다.

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Import;
 
 //@Import(AppV2Config.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
